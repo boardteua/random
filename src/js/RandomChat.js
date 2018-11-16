a128bf6960ddc347b9efc3ab304fddd733479e0d
org100h <myrror555@gmail.com>
2018-11-16 21:17:15 +0200

    mod template

--- src/js/RandomChat.js
+++ src/js/RandomChat.js
@@ -69,7 +69,7 @@ function ChatInput(input) {
 
     this.editable = function (bool) {
         input.getElementsByClassName("chat-input")[0].setAttribute("contenteditable", bool);
-        input.className = (bool)?'container input-wraper enabled':"container input-wraper disabled";
+        input.getElementsByClassName("chat-input")[0].className = (bool)?'container enabled':"container disabled";
     };
 
     this.enter = function (func) {
