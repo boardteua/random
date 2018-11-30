a5e47bf69e2f2897884dfa620abcd9bf118b99e9
org100h <myrror555@gmail.com>
2018-11-30 12:22:49 +0000

    Update soud msg

--- src/js/RandomChat.js
+++ src/js/RandomChat.js
@@ -1,3 +1,50 @@
+function BufferLoader(context, urlList, callback) {
+  this.context = context;
+  this.urlList = urlList;
+  this.onload = callback;
+  this.bufferList = new Array();
+  this.loadCount = 0;
+}
+
+BufferLoader.prototype.loadBuffer = function(url, index) {
+  // Load buffer asynchronously
+  var request = new XMLHttpRequest();
+  request.open("GET", url, true);
+  request.responseType = "arraybuffer";
+
+  var loader = this;
+
+  request.onload = function() {
+    // Asynchronously decode the audio file data in request.response
+    loader.context.decodeAudioData(
+      request.response,
+      function(buffer) {
+        if (!buffer) {
+          alert('error decoding file data: ' + url);
+          return;
+        }
+        loader.bufferList[index] = buffer;
+        if (++loader.loadCount == loader.urlList.length)
+          loader.onload(loader.bufferList);
+      },
+      function(error) {
+        console.error('decodeAudioData error', error);
+      }
+    );
+  }
+
+  request.onerror = function() {
+    alert('BufferLoader: XHR error');
+  }
+
+  request.send();
+}
+
+BufferLoader.prototype.load = function() {
+  for (var i = 0; i < this.urlList.length; ++i)
+  this.loadBuffer(this.urlList[i], i);
+}
+
 function ChatLog(log) {
     "use strict";
 
@@ -45,21 +92,34 @@ function ChatLog(log) {
         scroll();
     };
 }
-function PlaySound(sound) {
-    switch (sound) {
-        case 'newpair':
-            var audio = new Audio('sound/newpair.mp3');
-            break;
-        case 'newmsg':
-            var audio = new Audio('sound/stairs.mp3');
-            break;
-    }
-    audio.pause();
-    setTimeout(function () {
-        audio.play();
-    }, 0);
-    
+
+
+var context;
+var bufferLoader;
+
+function PlayInit(sound) {
+
+    window.AudioContext = window.AudioContext || window.webkitAudioContext;
+    context = new AudioContext();
+    bufferLoader = new BufferLoader(
+    context,
+    [
+      'sound/' + sound +  '.mp3',
+    ],
+    playSound
+    );
+
+    bufferLoader.load();
 }
+
+function playSound(bufferList, sound) {
+    var source = context.createBufferSource();
+	source.buffer = bufferList[0];
+        source.connect(context.destination);
+        source.start(0);
+   
+}
+
 
 function ChatInput(input) {
     "use strict";
@@ -145,11 +205,13 @@ function RandomChat(url, win) {
                 log.systemMessage("Знайшов!");
                 input.editable(true);
                 input.focus();
-                PlaySound('newpair');
+                PlayInit('newpair');
+                context.resume();
                 break;
             case "message":
                 log.partnerMessage(msg.text);
-				PlaySound('newmsg');
+		PlayInit('stairs');
+                context.resume();
                 break;
             case "disconnected":
                 log.systemMessage(
